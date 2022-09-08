9dc79a91f159b8b015f148174b6e27e45d8c343d
Oscar Hernandez <oscarhernandez@Mac-mini-de-Administrador.local>
2022-09-08 11:21:40 +0200

    SimpleGenerator

---
+++ gen.php
@@ -0,0 +1,54 @@
+<?php
+
+include 'qrcode.php';
+
+function data_provider($n, $hash_lenght=10) 
+{
+    $set = [];
+
+    while(count($set) != $n) 
+    {
+        $hash = substr(str_shuffle(MD5(microtime())), 0, 10);
+        if(in_array($hash, $set)) 
+        {
+            continue;
+        }
+        $set[] = $hash;
+    }
+    return $set;
+}
+
+
+function create_QR($path, $filename, $data) {
+    $options = [];
+    $generator = new QRCode($data, $options);
+
+    /* Create bitmap image. */
+    $image = $generator->render_image();
+    imagepng($image, $path . "/" . $filename . '.png');
+    imagedestroy($image);
+}
+
+
+function gen($path, $gen_amount) 
+{
+    $data = data_provider($gen_amount);
+
+    for($i = 0; $i < count($data); $i++)
+    {
+        create_QR($path, $data[$i], $data[$i]);
+    }
+}
+
+
+$path = __DIR__."/".$argv[1];
+$gen_amount = $argv[2];
+
+if (!file_exists($path)) {
+    mkdir($path, 0777, true);
+}
+
+gen($path, $gen_amount);
+
+
+?>
